e73c6a10ad96d9cd7b54b29fdeb21277358bdf2e
“Anderson <anderson_chenko@hotmail.com>
2020-02-08 21:38:47 -0300

    criado seed artista album

--- database/seeds/ArtistaAlbumTableSeeder.php
+++ database/seeds/ArtistaAlbumTableSeeder.php
@@ -11,6 +11,184 @@ class ArtistaAlbumTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 2,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 3,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 4,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 5,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 6,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 7,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 8,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 9,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 1,
+            'id_album' => 10,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 11,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 12,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 13,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 14,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 15,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 16,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 17,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 18,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 19,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 2,
+            'id_album' => 20,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 21,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 22,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 23,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 24,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 25,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 26,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 27,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 28,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 29,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('artista_album')->insert([
+            'id_artista' => 3,
+            'id_album' => 30,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
     }
 }
